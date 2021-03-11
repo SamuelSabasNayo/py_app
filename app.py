50ce77e9fa35baf04a710ee9e9fc0c929c05512a
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-03-11 10:40:51 +0200

    formatted string

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
-name = input('What is your name? ')
-color = input('What is your favorite color? ')
-print(name + ' likes ' + color)
+first = 'John'
+last = 'Smith'
+
+msg = f'{first} [{last}] is a coder'
+
+print(msg)
+
+# course = 'Python for Beginners'
+# another = course[:]
+
+# print(another)
+
+# course = '''
+#   Hi John,
+#   Here is our first email to you.
+#   Thank you!!
+# '''
+
+# print(course)
+
+# weight_kg = input('Weight (kg): ')
+# weight_lbs = int(weight_kg) / 0.45
+
+# print(weight_lbs)
+
+# year = input('Birth year: ')
+# print(type(year))
+# age = 2021 - int(year)
+# print(type(age))
+# print(age)
+
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print(name + ' likes ' + color)
 
 
 # name = input('What is your name? ')
@@ -10,3 +41,5 @@ print(name + ' likes ' + color)
 # print(price)
 
 # print('Hello Sam')
+
+# Beignner thing
