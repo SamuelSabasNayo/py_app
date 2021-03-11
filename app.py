34f3118cdc4b4a5a6c3c811af236a348b745a14f
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-03-11 14:54:26 +0200

    if statements

--- app.py
+++ app.py
@@ -1,6 +1,64 @@
-import math
+has_high_income = True
+has_criminal_record = False
 
-print(math.pow(4, 3))
+if has_high_income and not has_criminal_record:
+  print('Eligible for loan')
+
+  
+# has_high_income = True
+# has_good_credit = False
+
+# if has_high_income or has_good_credit:
+#   print('Eligible for loan')
+
+  
+# has_high_income = True
+# has_good_credit = True
+
+# if has_high_income and has_good_credit:
+#   print('Eligible for loan')
+
+# price = 100
+# has_good_credit = False
+
+# if has_good_credit:
+#   down_payment = 0.1 * price
+# else:
+#   down_payment = 0.2 * price
+# print(f"Down Payment: {down_payment}")
+
+
+# is_hot = False
+# is_cold = False
+
+# if is_hot:
+#   print("It's a hot day.")
+#   print("Drink plenty of water")
+# elif is_hot:
+#   print("It's a cold day")
+#   print("Wear warm clothes")
+# else:
+#   print("It's a lovely day.")
+# print("Enjoy your day.")
+
+
+# is_hot = False
+
+# if is_hot:
+#   print("It's a hot day.")
+# print("Enjoy your day.")
+
+
+# is_hot = True
+
+# if is_hot:
+#   print("It's a hot day.")
+# print("Enjoy your day.")
+
+
+# import math
+
+# print(math.pow(4, 3))
 
 
 # import math
